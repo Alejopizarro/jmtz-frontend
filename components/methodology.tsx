7be64887bf111ee159a123283c7c0e6ee6111681
Alejopizarro <alejomkt10@gmail.com>
2025-10-07 22:37:59 +0200

    fix px methodology

--- components/methodology.tsx
+++ components/methodology.tsx
@@ -33,7 +33,7 @@ const methodologySteps: MethodologyStep[] = [
 
 const Methodology = () => {
   return (
-    <section className="bg-background py-12 px- lg:py-26">
+    <section className="bg-background py-12 px-4 lg:py-26">
       <div className="max-w-7xl mx-auto bg-gradient-to-br from-slate-50 to-slate-300 text-transparent bg-clip-text">
         <h2 className="text-2xl lg:text-4xl font-light mb-4">
           Our Methodology
